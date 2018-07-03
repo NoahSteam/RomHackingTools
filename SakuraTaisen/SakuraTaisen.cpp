418780f21b783bac7851830f3dc31fc6b4e7ad4b
NoahSteam <rizzawan@yahoo.com>
2018-07-03 18:24:10 -0500

    TMapSpreadsheet

--- SakuraTaisen/SakuraTaisen.cpp
+++ SakuraTaisen/SakuraTaisen.cpp
@@ -1926,6 +1926,201 @@ void OutputDialogOrder(const string& rootSakuraTaisenDirectory, const string& ou
 	}	
 }
 
+bool CreateTMapSpSpreadsheet(const string& imageDirectory)
+{
+	const int tmapImages[] = 
+	{
+		53,
+		54,
+		55,
+		59,
+		60,
+		63,
+		64,
+		67,
+		70,
+		77,
+		78,
+		79,
+		80,
+		81,
+		82,
+		83,
+		84,
+		85,
+		88,
+		90
+	};
+
+	TextFileWriter htmlFile;
+	const string htmlFileName = imageDirectory + string("..\\..\\Translation\\TMapSP.php");
+	if( !htmlFile.OpenFileForWrite(htmlFileName) )
+	{
+		printf("Unable to create an html file: %s", htmlFileName.c_str());
+		return false;
+	}
+
+	htmlFile.WriteString("<html>\n");
+	htmlFile.WriteString("	<head><style>textarea {width: 100%;top: 0; left: 0; right: 0; bottom: 0; position: absolute; resize: none;-webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;} table {border-collapse: collapse;} table, th, td { position: relative; border: 1px solid black;}#myProgress {width: 100%;	background-color: #ddd;} #myBar {width: 1%;height: 30px; background-color: #4CAF50;}</style>\n");
+	htmlFile.WriteString("	<div id=\"FileName\" style=\"display: none;\">TMapSP</div>\n");
+	htmlFile.WriteString("	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\">\n");
+	htmlFile.WriteString("		$( window ).on( \"load\", function()\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			OnStartup();\n");
+	htmlFile.WriteString("		});\n");
+	htmlFile.WriteString("	</script>\n\n");
+	htmlFile.WriteString("	<script type=\"text/javascript\">\n");
+	htmlFile.WriteString("		function SaveData(inDialogImageName, inDivID)\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			var translatedText = document.getElementById(inDivID).value;\n");
+	htmlFile.WriteString("			var fileName = document.getElementById(\"FileName\").innerHTML;\n");
+	htmlFile.WriteString("			$.ajax({\n");
+	htmlFile.WriteString("			type: \"POST\",\n");
+	htmlFile.WriteString("			url: \"UpdateTranslationTest.php\",\n");
+	htmlFile.WriteString("			data: { inTBLFileName: fileName, inImageName:inDialogImageName, inTranslation:translatedText, inDivId:inDivID, inCrc:0 },\n");
+	htmlFile.WriteString("			success: function(result)\n");
+	htmlFile.WriteString("			{\n");
+	htmlFile.WriteString("				var trId = \"tr_\" + inDivID;\n");
+	htmlFile.WriteString("				if( translatedText != \"Untranslated\" && translatedText != \"<div>Untranslated</div>\")\n");
+	htmlFile.WriteString("				{\n");
+	htmlFile.WriteString("					if( document.getElementById(trId).bgColor != \"#fec8c8\" )\n");
+	htmlFile.WriteString("					{\n");
+	htmlFile.WriteString("						document.getElementById(trId).bgColor = \"#e3fec8\";\n");
+	htmlFile.WriteString("					}\n");
+	htmlFile.WriteString("				}\n");
+	htmlFile.WriteString("				else\n");
+	htmlFile.WriteString("				{\n");
+	htmlFile.WriteString("					document.getElementById(trId).bgColor = \"#fefec8\";\n");
+	htmlFile.WriteString("				}\n");
+	htmlFile.WriteString("			}\n");
+	htmlFile.WriteString("		});\n");
+	htmlFile.WriteString("	}\n");
+	htmlFile.WriteString("		function SaveEdits(inDialogImageName, inDivID)\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			SaveData(inDialogImageName, inDivID);\n");
+	htmlFile.WriteString("		}\n");
+	htmlFile.WriteString("		function ExportData()\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("		$(\"textarea\").each ( function ()\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			var thisText = $(this).text();\n");
+	htmlFile.WriteString("			thisText = thisText.replace(/<br>/g, '&ltbr&gt');\n");
+	htmlFile.WriteString("			thisText = thisText.replace(/<sp>/g, '&ltsp&gt');\n");
+	htmlFile.WriteString("			document.write(thisText + \"<br>\");\n");
+	htmlFile.WriteString("		});\n");
+	htmlFile.WriteString("	}\n");
+	htmlFile.WriteString("	function LoadData()\n");
+	htmlFile.WriteString("	{\n");
+	htmlFile.WriteString("		var fileName = document.getElementById(\"FileName\").innerHTML;\n");
+	htmlFile.WriteString("		$.ajax({\n");
+	htmlFile.WriteString("		type: \"POST\",\n");
+	htmlFile.WriteString("			  url: \"GetTranslationData.php\",\n");
+	htmlFile.WriteString("				data: { inTBLFileName: fileName},\n");
+	htmlFile.WriteString("				success: function(result)\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			var json = $.parseJSON(result);\n");
+	htmlFile.WriteString("			var i;\n");
+	htmlFile.WriteString("			for (i = 0; i < json.length; i++)\n");
+	htmlFile.WriteString("			{\n");
+	htmlFile.WriteString("				var jsonEntry = json[i];\n");
+	htmlFile.WriteString("				var english   = jsonEntry.English.replace(/\\/g, '');\n");
+	htmlFile.WriteString("				var divId     = \"#\" + jsonEntry.DivId;\n");
+	htmlFile.WriteString("					var trId      = \"tr_\" + jsonEntry.DivId;\n");
+	htmlFile.WriteString("					if( english != \"Untranslated\" && english != \"<div>Untranslated</div>\")\n");
+	htmlFile.WriteString("					{\n");
+	htmlFile.WriteString("						$(divId).html(english);\n");
+	htmlFile.WriteString("					}\n");
+	htmlFile.WriteString("			}\n");
+	htmlFile.WriteString("		},\n");
+	htmlFile.WriteString("		error: function()\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			alert('Unable to load data');\n");
+	htmlFile.WriteString("		}\n");
+	htmlFile.WriteString("		});\n");
+	htmlFile.WriteString("	}\n");
+	htmlFile.WriteString("	function FixOnChangeEditableElements()\n");
+	htmlFile.WriteString("	{\n");
+	htmlFile.WriteString("		var tags = document.querySelectorAll('[contenteditable=true][onChange]');\n");
+	htmlFile.WriteString("		for (var i=tags.length-1; i>=0; i--) if (typeof(tags[i].onblur)!='function')\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			tags[i].onfocus = function()\n");
+	htmlFile.WriteString("			{\n");
+	htmlFile.WriteString("				this.data_orig=this.innerHTML;\n");
+	htmlFile.WriteString("			};\n");
+	htmlFile.WriteString("			tags[i].onblur = function()\n");
+	htmlFile.WriteString("			{\n");
+	htmlFile.WriteString("				if( this.innerHTML != this.data_orig)\n");
+	htmlFile.WriteString("					this.onchange();\n");
+	htmlFile.WriteString("				delete this.data_orig;\n");
+	htmlFile.WriteString("			};\n");
+	htmlFile.WriteString("		}\n");
+	htmlFile.WriteString("	}\n");
+	htmlFile.WriteString("	function OnStartup()\n");
+	htmlFile.WriteString("	{\n");
+	htmlFile.WriteString("		FixOnChangeEditableElements();\n");
+	htmlFile.WriteString("	}\n");
+	htmlFile.WriteString("	</script>\n");
+	htmlFile.WriteString("	</head>\n");
+	htmlFile.WriteString("	\n");
+	htmlFile.WriteString("	<body>\n");
+	htmlFile.WriteString("	<?php include 'GetUserPermissions.php';\n");
+	htmlFile.WriteString("	$bPermissionFound = false;\n");
+	htmlFile.WriteString("	foreach ($allowedFiles as $value)\n");
+	htmlFile.WriteString("	{\n");
+	htmlFile.WriteString("		if( $value == \"0100TBL\" )\n");
+	htmlFile.WriteString("		{\n");
+	htmlFile.WriteString("			$bPermissionFound = true;\n");
+	htmlFile.WriteString("			break;\n");
+	htmlFile.WriteString("		}\n");
+	htmlFile.WriteString("	}\n");
+	htmlFile.WriteString("	if( $bPermissionFound )\n\t{\n?>");
+	htmlFile.WriteString("		<article><header align=\"center\"><h1>File: TMapSP</h1></header></article>\n");
+	htmlFile.WriteString("			<br>\n");
+	htmlFile.WriteString("			<b>Instructions:</b><br>\n");
+	htmlFile.WriteString("			-This page is best displayed using Chrome.  Otherwise some of the table borders are missing for some reason.<br>\n");
+	htmlFile.WriteString("			-Your changes are automatically saved.<br>\n");
+	htmlFile.WriteString("			-Press the Load Data button when you come back to the page to load your changes.<br>\n");
+	htmlFile.WriteString("			<b>Naming Conventions:</n><br>\n");
+	htmlFile.WriteString("			<a href=\"https://docs.google.com/spreadsheets/d/1rgafQe78vML_xbxnYuOSlO8P5C8nuhgLjMJOExUQsm0/edit?usp=sharing\" target=\"_blank\">Click here to view the naming conventions for Characters, Locations, and Terms</a> <br>\n");
+	htmlFile.WriteString("			<?php\n");
+	htmlFile.WriteString("				$currUser = $_SERVER['PHP_AUTH_USER'];\n");
+	htmlFile.WriteString("				if( $currUser == \"swtranslator\" )\n");
+	htmlFile.WriteString("				{\n");
+	htmlFile.WriteString("					echo \"<input align=\\\"center\" type=\\\"button\\\" value=\\\"Export Data\\\" onclick=\\\"ExportData()\\\"/>\";\n");
+	htmlFile.WriteString("				}\n");
+	htmlFile.WriteString("			?>\n\n");
+	htmlFile.WriteString("			<table align=\"center\">\n");
+	htmlFile.WriteString("				<tr>\n");
+	htmlFile.WriteString("					<td>\n");
+	htmlFile.WriteString("						<input align=\"center\" type=\"button\" value=\"Load Data\" onclick=\"LoadData()\"/>\n");
+	htmlFile.WriteString("					</td>\n");
+	htmlFile.WriteString("				</tr>\n");
+	htmlFile.WriteString("			</table><br>\n\n");
+	htmlFile.WriteString("			<table>\n");
+	htmlFile.WriteString("				<tr bgcolor=\"#c8c8fe\">\n");
+	htmlFile.WriteString("					<th>#</th>\n");
+	htmlFile.WriteString("					<th>Japanese</th>\n");
+	htmlFile.WriteString("					<th>English</th>\n");
+	htmlFile.WriteString("				</tr>\n");
+	//TODO iterate through all images
+	const int numImages = sizeof(tmapImages)/sizeof(int);
+	for(int i = 0; i < numImages; ++i)
+	{
+		fprintf(htmlFile.GetFileHandle(), "				<tr id=\"tr_edit_%i\" bgcolor=\"#fefec8\">\n", tmapImages[i]);
+		fprintf(htmlFile.GetFileHandle(), "					<td align=\"center\" width=\"20\">%i</td><td width=\"88\"><img src=\"..\\ExtractedData\\TMapSP\\%i.png\"></td><td width=\"480\"><textarea id=\"edit_%i\" contenteditable=true onchange=\"SaveEdits('%i.bmp', 'edit_%i')\" style=\"border: none; width: 100%%; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;\">Untranslated</textarea></td>\n", tmapImages[i], tmapImages[i], tmapImages[i], tmapImages[i], tmapImages[i]);
+		fprintf(htmlFile.GetFileHandle(), "				</tr>\n");
+	}
+	
+	htmlFile.WriteString("			</table><br>\n");
+	htmlFile.WriteString("			<?php\n");
+	htmlFile.WriteString("		}\n");
+	htmlFile.WriteString("	?></body>\n");
+	htmlFile.WriteString("	</html>\n");
+
+	return true;
+
+}
+
 bool CreateTBLSpreadsheets(const string& dialogImageDirectory, const string& duplicatesFileName, const string& sakura1Directory)
 {
 	printf("Parsing duplicates file\n");
@@ -2737,10 +2932,6 @@ void ExtractTMapSP(const string& sakuraDirectory, const string& paletteFileName,
 		lookupTable[i].addressInTmapSP = SwapByteOrder(lookupTable[i].addressInTmapSP);
 
 		const string outFileName = outDirectory + std::to_string(i);
-		const string outFileNameTiled = outDirectory + std::to_string(i) + string("_tiled");
-
-		FileNameContainer outFileNameInfo(outFileName.c_str());
-		ExtractImage(tmapFileNameInfo, outFileNameTiled, paletteFile, lookupTable[i].width*8, lookupTable[i].height, 1, lookupTable[i].addressInTmapSP*8, false);
 		
 		BitmapWriter outBmp;
 		outBmp.CreateBitmap(outFileName, lookupTable[i].width*8, lookupTable[i].height, 4, tmapFileData.GetData() + lookupTable[i].addressInTmapSP*8, (lookupTable[i].width*lookupTable[i].height)/2, paletteData.GetData(), paletteData.GetSize());
@@ -2824,6 +3015,7 @@ void PrintHelp()
 	printf("FindDuplicateText dialogDirectory outFile\n");
 	printf("FindDialogOrder rootSakuraTaisenDirectory outDirectory\n");
 	printf("CreateTBLSpreadsheets dialogImageDirectory duplicatesFile sakura1Directory\n");
+	printf("CreateTMapSpSpreadsheet imageDirectory\n");
 	printf("ExtractImages fileName paletteFile width height outDirectory\n");
 	printf("Extract8BitImage fileName paletteFile offset outDirectory\n");
 	printf("ExtractFaceFiles rootSakuraTaisenDirectory paletteFile outDirectory\n");
@@ -2951,6 +3143,12 @@ int main(int argc, char *argv[])
 		const string sakura1Directory     = string(argv[4]) + Seperators;
 
 		CreateTBLSpreadsheets(dialogImageDirectory, duplicatesFile, sakura1Directory);
+	}
+	else if(command == "CreateTMapSpSpreadsheet" && argc == 3 )
+	{
+		const string imageDirectory = string(argv[2]);
+
+		CreateTMapSpSpreadsheet(imageDirectory);
 	}
 	else if(command == "Extract8BitImage" && argc == 6 )
 	{
