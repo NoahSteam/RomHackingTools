57a9f94a2fcde53bf69a52bc706854f9c424bb22
NoahSteam <rizzawan@yahoo.com>
2019-12-17 20:35:02 -0600

    random data that should be checke din

---
+++ SakuraTaisen/Data/SourceArt/VDP2/IntroLogo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="IntroLogo" tilewidth="8" tileheight="8" tilecount="133" columns="19">
+ <image source="../../SakuraLogo_Sakura.bmp" width="152" height="56"/>
+</tileset>
---
+++ SakuraTaisen/Data/SourceArt/VDP2/IntroLogoSmall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="IntroLogoSmall" tilewidth="8" tileheight="8" tilecount="85" columns="17">
+ <image source="../../SakuraLogo_Small.bmp" width="140" height="40"/>
+</tileset>
