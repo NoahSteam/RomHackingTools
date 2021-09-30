ca7085851e89bea6bd7c9812e8b5d80a0fe84f4f
LenovoLaptop <rizzawan@yahoo.com>
2021-09-30 00:16:20 -0500

    csv reader

--- DragonForce/Source/DragonForce.cpp
+++ DragonForce/Source/DragonForce.cpp
@@ -507,6 +507,83 @@ bool CopyOriginalFiles(const string& inDiscDirectory, const string& inPatchedDis
 	return true;
 }
 
+class DragonForceCSVReader
+{
+public:
+
+	struct LineEntry
+	{
+		string japanese;
+		string google;
+		string english;
+
+		void Clear()
+		{
+			japanese.clear();
+			google.clear();
+			english.clear();
+		}
+	};
+
+	vector<LineEntry> lines;
+
+	bool InitializeFile(const string& fileName)
+	{
+		FileNameContainer fileNameInfo(fileName.c_str());
+		FileData fileData;
+		if( !fileData.InitializeFileData(fileNameInfo) )
+		{
+			return false;
+		}
+
+		uint32 index = 0;
+		const uint32 fileSize = fileData.GetDataSize();
+		const char* pData = fileData.GetData();
+		int entryType = 0;
+		LineEntry entry;
+		char buffer[2048];
+		memset(buffer, 0, sizeof(buffer));
+		uint32 bufferIndex = 0;
+
+		while( index < fileSize )
+		{
+			if( pData[index] == ',' )
+			{
+				if( entryType == 0 )
+				{
+					entry.japanese = string(buffer);
+				}
+				else if( entryType == 1 )
+				{
+					entry.google = string(buffer);
+				}
+				else
+				{
+					entry.english = string(buffer);
+				}
+				memset(buffer, 0, sizeof(buffer));
+				bufferIndex = 0;
+
+				++entryType;
+			}
+			else if( pData[index] == 0xA )
+			{
+				lines.push_back(entry);
+				entry.Clear();
+
+				memset(buffer, 0, sizeof(buffer));
+				bufferIndex = 0;
+				entryType = 0;
+			}
+			else
+			{
+				buffer[bufferIndex++] = pData[index];
+			}
+			++index;
+		}
+	}
+};
+
 int main(int argc, char* argv[])
 {
 	if (argc == 1)
@@ -514,6 +591,9 @@ int main(int argc, char* argv[])
 //		PrintHelp();
 		return 1;
 	}
+
+	DragonForceCSVReader reader;
+	reader.InitializeFile("C:\\Users\\16306\\Desktop\\Rizwan\\RomHackingTools\\DragonForce\\Data\\Translation\\Ready.csv");
 
 	const string command(argv[1]);
 
