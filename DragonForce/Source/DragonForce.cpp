eda0658983f92935cd26e1c2b183d23ee0345f12
LenovoLaptop <rizzawan@yahoo.com>
2021-09-30 22:05:29 -0500

    csv reader

--- DragonForce/Source/DragonForce.cpp
+++ DragonForce/Source/DragonForce.cpp
@@ -31,6 +31,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <assert.h>
 
 #include "..\..\Utils\Utils.h"
+#include "DragonForceCSVReader.h"
 
 using std::vector;
 using std::string;
@@ -439,6 +440,23 @@ void ExtractTextFromStream(const char* pInData, uint32 inOffset, uint32 inDataSi
 	}
 }
 
+bool PatchTextInFile(const string& inCsvFilePath, const string& inPatchedFilePath)
+{
+	DragonForceCSVReader csvReader;
+	if( !csvReader.InitializeFile(inCsvFilePath) )
+	{
+		return false;
+	}
+
+	FileData dataFile;
+	if( !dataFile.InitializeFileData(FileNameContainer(inPatchedFilePath.c_str())) )
+	{
+		return false;
+	}
+
+	
+}
+
 bool MatchUpTextFromReadyFiles(const string& inPs2ReadyFilePath, const string& inSaturnEngReadyFilePath, const string& inSaturnJpReadyFilePath,
 							   const string& inOutputDirectory)
 {
@@ -506,83 +524,6 @@ bool CopyOriginalFiles(const string& inDiscDirectory, const string& inPatchedDis
 
 	return true;
 }
-
-class DragonForceCSVReader
-{
-public:
-
-	struct LineEntry
-	{
-		string japanese;
-		string google;
-		string english;
-
-		void Clear()
-		{
-			japanese.clear();
-			google.clear();
-			english.clear();
-		}
-	};
-
-	vector<LineEntry> lines;
-
-	bool InitializeFile(const string& fileName)
-	{
-		FileNameContainer fileNameInfo(fileName.c_str());
-		FileData fileData;
-		if( !fileData.InitializeFileData(fileNameInfo) )
-		{
-			return false;
-		}
-
-		uint32 index = 0;
-		const uint32 fileSize = fileData.GetDataSize();
-		const char* pData = fileData.GetData();
-		int entryType = 0;
-		LineEntry entry;
-		char buffer[2048];
-		memset(buffer, 0, sizeof(buffer));
-		uint32 bufferIndex = 0;
-
-		while( index < fileSize )
-		{
-			if( pData[index] == ',' )
-			{
-				if( entryType == 0 )
-				{
-					entry.japanese = string(buffer);
-				}
-				else if( entryType == 1 )
-				{
-					entry.google = string(buffer);
-				}
-				else
-				{
-					entry.english = string(buffer);
-				}
-				memset(buffer, 0, sizeof(buffer));
-				bufferIndex = 0;
-
-				++entryType;
-			}
-			else if( pData[index] == 0xA )
-			{
-				lines.push_back(entry);
-				entry.Clear();
-
-				memset(buffer, 0, sizeof(buffer));
-				bufferIndex = 0;
-				entryType = 0;
-			}
-			else
-			{
-				buffer[bufferIndex++] = pData[index];
-			}
-			++index;
-		}
-	}
-};
 
 int main(int argc, char* argv[])
 {
