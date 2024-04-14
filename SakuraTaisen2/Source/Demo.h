d3e33938a3ad28eb8b8272154cf71df19755603d
NoahSteam <rizzawan@yahoo.com>
2024-04-14 16:25:55 -0500

    reworking demo file wip

--- SakuraTaisen2/Source/Demo.h
+++ SakuraTaisen2/Source/Demo.h
@@ -116,6 +116,89 @@ void ExtractDemoWPALL(const std::string& inDemoDirectory, const std::string& inO
 	}
 }
 
+void ExtractLoadScreens(const std::string& inDemoDirectory, const std::string& inOutputDirectory)
+{
+	CreateDirectoryHelper(inOutputDirectory);
+
+	//N_Load
+	{
+		const std::string nLoadPath = inDemoDirectory + "N_LOAD.ALL";
+		FileData nLoadData;
+		if (!nLoadData.InitializeFileData(nLoadPath))
+		{
+			return;
+		}
+
+		const std::string wpallPath = inDemoDirectory + "WPALL1.ALL";
+		FileData wpallData;
+		if (!wpallData.InitializeFileData(wpallPath))
+		{
+			return;
+		}
+
+		for (int i = 0; i < 3; ++i)
+		{
+			FileData* pFileData = TiledLoadScreenImages[i].bIsWpall ? &wpallData : &nLoadData;
+
+			SakuraFontSheet tileSheet;
+			if (!tileSheet.CreateFontSheetFromData(pFileData->GetData() + TiledLoadScreenImages[i].colorOffset, TiledLoadScreenImages[i].colorSize, 8, 8, false))
+			{
+				return;
+			}
+
+			//Create 32bit palette data
+			PaletteData paletteData;
+			paletteData.CreateFrom15BitData(pFileData->GetData() + TiledLoadScreenImages[i].paletteOffset, 512);
+
+			BitmapSurface sakuraStringBmp;
+			sakuraStringBmp.CreateSurface(320, 224, BitmapSurface::EBitsPerPixel::kBPP_8, paletteData.GetData(), paletteData.GetSize());
+
+			const string bitmapFileName = inOutputDirectory + std::string("TileSheet") + std::to_string(i) + ".bmp";
+			ExtractImageFromData(pFileData->GetData() + TiledLoadScreenImages[i].colorOffset, 8 * 8 * (320 / 8) * (224 / 8), bitmapFileName, pFileData->GetData() + TiledLoadScreenImages[i].paletteOffset, 512, false, 8, 8, 320 / 8, 256, 0, true, true);
+
+			unsigned int tiles[1120];
+			memcpy_s(tiles, sizeof(tiles), pFileData->GetData() + TiledLoadScreenImages[i].tileOffset, sizeof(tiles));
+			int x = 0;
+			int y = 0;
+
+			for (int t = 0; t < 1120; ++t)
+			{
+				const int tileValue = (SwapByteOrder(tiles[t]) / 2) - 1;
+				tiles[t] = tileValue;
+			}
+
+			for (int t = 0; t < 1120; ++t)
+			{
+				SakuraString::SakuraChar sakuraChar;
+				sakuraChar.mIndex = tiles[t];
+
+				const char* pTileData = tileSheet.GetCharacterTile(sakuraChar);
+				sakuraStringBmp.AddTile(pTileData, 64, x, y, 8, 8, BitmapSurface::kFlipNone);
+
+				x += 8;
+				if (x == 320)
+				{
+					x = 0;
+					y += 8;
+				}
+			}
+
+			std::string outFile;
+			if (TiledLoadScreenImages[i].bIsWpall)
+			{
+				outFile = inOutputDirectory + "wpall_" + std::to_string(i) + ".bmp";
+			}
+			else
+			{
+				outFile = inOutputDirectory + "nload_" + std::to_string(i) + ".bmp";
+			}
+
+			sakuraStringBmp.WriteToFile(outFile, true);
+		}
+	}
+}
+
+
 bool PatchLoadScreens(const std::string& inPatchedSakuraDirectory, const std::string& inTranslatedDataDirectory)
 {
 	const std::string nLoadPath = inPatchedSakuraDirectory + "SAKURA1\\N_LOAD.ALL";
