74a0c985206fd4483528c7cc7c87c44bd19351fc
NoahSteam <rizzawan@yahoo.com>
2024-10-25 16:25:10 -0500

    Tycoon tutorial wip

--- SakuraTaisen2/Source/TycoonTutorial.h
+++ SakuraTaisen2/Source/TycoonTutorial.h
@@ -169,11 +169,233 @@ bool PatchTycoonTutorialRenderingCode(const string& inOriginalDirectory, const s
 	return true;
 }
 
+void CreatePatchedTycoonTutorialLine(const TextFileData::TextLine& InTextLine, const int InTranslatedLineIndex, vector<SakuraString>& OutLines)
+{
+#define IncrementTycoonLine()\
+    ++currLine;\
+    charCount = 0;\
+    if( currLine > maxLines ) \
+	{\
+		printf("Doesn't Fit: %s\n", InTextLine.mFullLine.c_str());\
+		bFailedToAddLine = true;\
+	}\
+    translatedString.AddChar( GTranslationLookupTable.GetIndex('\n') );
+
+	//Insert new line if needed
+#define ConditionallyAddNewTycoonLine()\
+	if( word.size() + charCount > MaxCharsPerLine )\
+	{\
+		if( !bAlreadyShowedError )\
+		{\
+		}\
+		else\
+		{\
+			IncrementTycoonLine()\
+		}\
+	}
+
+	SakuraString translatedString;
+	int charCount = 0;
+	int currLine = 0;
+	const int maxLines = 4;
+	bool bAlreadyShowedError = false;
+
+	const size_t numWords = InTextLine.mWords.size();
+	for (size_t wordIndex = 0; wordIndex < numWords; ++wordIndex)
+	{
+		const string& word = InTextLine.mWords[wordIndex];
+		bool bFailedToAddLine = false;
+		if (word.size() > MaxCharsPerLine)
+		{
+			printf("Unable to insert word because it is longer than %i characters: %s[%zi]\n", MaxCharsPerLine, word.c_str(), word.size());
+			continue;
+		}
+
+		//Check to see if this is a newline
+		const bool bNewLineWord = word == NewLineWord;
+		if (bNewLineWord)
+		{
+			IncrementTycoonLine();
+
+			//bFailedToAddLine set inside IncrementLine
+			if (bFailedToAddLine)
+			{
+				//	break;
+			}
+
+			continue;
+		}
+
+		//Check to see if this is a space
+		const bool bSpaceWord = word == SpaceWord;
+		if (bSpaceWord)
+		{
+			if (charCount + 1 > MaxCharsPerLine)
+			{				
+				IncrementTycoonLine();
+			
+				//bFailedToAddLine set inside IncrementLine
+				if (bFailedToAddLine)
+				{
+					//	break;
+				}
+			}
+			else
+			{
+				translatedString.AddChar(GTranslationLookupTable.GetIndex(' '));
+				++charCount;
+			}
+
+			continue;
+		}
+		else if (word == IgnoreCharacter)
+		{
+			continue;
+		}
+
+		//Insert new line if needed
+		ConditionallyAddNewTycoonLine();
+
+		//Add the word
+		const size_t numLettersInWord = word.size();
+		if (word.size() + charCount > MaxCharsPerLine)
+		{
+			IncrementTycoonLine();
+		}
+
+		const char* pWord = word.c_str();
+		for (size_t letterIndex = 0; letterIndex < numLettersInWord; ++letterIndex)
+		{
+			translatedString.AddChar(GTranslationLookupTable.GetIndex(pWord[letterIndex]));
+			++charCount;
+		}
+
+		//bFailedToAddLine is set inside ConditionallyAddNewLine
+		if (bFailedToAddLine)
+		{
+			break;
+		}
+
+		//Add space
+		if (wordIndex + 1 < numWords)
+		{
+			//Insert new line if needed
+			const string& nextWord = InTextLine.mWords[wordIndex + 1];
+			if (nextWord != NewLineWord)
+			{
+				//If adding the next word will put us over the word limit, add a new line
+				if (nextWord.size() + charCount > MaxCharsPerLine)
+				{
+					ConditionallyAddNewTycoonLine();
+				}
+				else //Otherwise add a space
+				{
+					if (charCount + 1 >= MaxCharsPerLine)
+					{						
+						IncrementTycoonLine();
+					}
+					else
+					{
+						translatedString.AddChar(GTranslationLookupTable.GetIndex(' '));
+						++charCount;
+					}
+				}
+			}
+		}
+	}//for(wordIndex < numWords)
+
+	OutLines.push_back(translatedString);
+}
+
+bool OutputPatchedTycoonTutorialText(const string& inPatchedDirectory, vector<SakuraString>& inTranslatedLines)
+{
+	FileWriter cardFile;
+	if (!cardFile.OpenFileForWrite(inPatchedDirectory + "\\SAKURA3\\CARD_DAT.ALL"))
+	{
+		return false;
+	}
+
+	vector<uint32> newEntryOffsets;
+	newEntryOffsets.push_back( SwapByteOrder((uint32)166)); //First value is the number of entries
+
+	//Output data
+	size_t dataIndex = 0;
+	size_t translationIndex = 0;
+	const size_t numInsertedLines = inTranslatedLines.size();
+	unsigned int numSingleBytesWritten = 0;
+	int entryOffset = 0x29c;
+	int offset = 0x31829c;
+	while (1)
+	{
+		if (translationIndex < numInsertedLines)
+		{
+			vector<unsigned char> translationData;
+			inTranslatedLines[dataIndex].GetSingleByteDataArray(translationData);
+			cardFile.WriteDataAtOffset(translationData.data(), translationData.size() * sizeof(char), offset);
+
+			const int numBytesWritten = (int)translationData.size() * sizeof(char);
+			numSingleBytesWritten += numBytesWritten;
+			offset += numBytesWritten;
+
+			newEntryOffsets.push_back(SwapByteOrder(entryOffset));
+
+			entryOffset += numBytesWritten;
+		}
+
+		++dataIndex;
+		++translationIndex;
+
+		if (translationIndex >= numInsertedLines)
+		{
+			break;
+		}
+	}
+
+	//Output new entry table
+	cardFile.WriteDataAtOffset((char*)newEntryOffsets.data(), newEntryOffsets.size()*sizeof(uint32), 0x318000);
+
+	return true;
+}
+
+bool PatchTycoonTutorialText(const string& inDataDirectory, const string& inPatchedDirectory)
+{
+	const FileNameContainer fileName(inDataDirectory + "Translation\\TycoonTutorial.txt");
+	TextFileData textFile(fileName);
+	if(!textFile.InitializeTextFile())
+	{
+		return false;
+	}
+
+	const size_t numLines = 166;
+	if(textFile.mLines.size() != numLines)
+	{
+		printf("Should be 166 lines in TycoonTutorial.txt\n");
+		return false;
+	}
+
+	vector<SakuraString> translatedLines;
+	for (size_t translatedLineIndex = 0; translatedLineIndex < numLines; ++translatedLineIndex)
+	{
+		const TextFileData::TextLine& textLine = textFile.mLines[translatedLineIndex];
+
+		CreatePatchedTycoonTutorialLine(textLine, translatedLineIndex, translatedLines);
+	}
+
+	OutputPatchedTycoonTutorialText(inPatchedDirectory, translatedLines);
+
+	return true;
+}
+
 bool PatchTycoonTutorial(const string& inOriginalDirectory, const string& inPatchedDirectory, const string& inDataDirectory, const TileExtractor& inFontSheet)
 {
 	printf("Patching Tycoon Tutorial\n");
 
 	if(!PatchTycoonTutorialRenderingCode(inOriginalDirectory, inPatchedDirectory))
+	{
+		return false;
+	}
+
+	if(!PatchTycoonTutorialText(inDataDirectory, inPatchedDirectory))
 	{
 		return false;
 	}
@@ -184,7 +406,6 @@ bool PatchTycoonTutorial(const string& inOriginalDirectory, const string& inPatc
 	{
 		return false;
 	}
-
 	inFontSheet.OutputTiles(cardFile, -1, 0x30e780 + 0x80);
 
 	return true;
