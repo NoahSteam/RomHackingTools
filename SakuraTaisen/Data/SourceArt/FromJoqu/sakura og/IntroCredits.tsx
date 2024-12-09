3688d5c8cdd6d9f267519e9e4a9b8fe1457d2c62
NoahSteam <rizzawan@yahoo.com>
2024-12-09 11:19:03 -0600

    Misc source files that should be added to depot

---
+++ SakuraTaisen/Data/SourceArt/FromJoqu/sakura og/IntroCredits.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="IntroCredits" tilewidth="8" tileheight="8" tilecount="300" columns="20">
+ <image source="../Documents/sakura wars cj iwakura/IntroCredits.bmp" width="160" height="120"/>
+</tileset>
